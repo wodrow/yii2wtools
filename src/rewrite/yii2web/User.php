9dd0149ad4b636952998e5aff3e856bfaaecb0da
wodrow-ys-pc <wodrow451611cv@gmail.com>
2019-09-24 14:27:11 +0800

    update

--- src/rewrite/yii2web/User.php
+++ src/rewrite/yii2web/User.php
@@ -12,7 +12,6 @@ class User extends \yii\web\User
 {
     public $isInConsole = false;
     public $loginIp = '';
-    public $loginMsg = '';
 
     /**
      * @param IdentityInterface $identity
@@ -23,19 +22,10 @@ class User extends \yii\web\User
     {
         if ($this->beforeLogin($identity, false, $duration)) {
             $this->switchIdentity($identity, $duration);
-            $user = $identity;
-            $this->loginMsg .= "{$user->username}";
             if ($this->isInConsole){
-                $this->loginIp = '127.0.0.1';
-                $this->loginMsg .= " 登录IP {$this->loginIp} 在控制";
+                $this->loginIp = '0.0.0.0';
             }else{
                 $this->loginIp = \Yii::$app->request->getUserIP();
-                $this->loginMsg .= " 登录IP {$this->loginIp}";
-            }
-            if ($this->enableSession) {
-                $this->loginMsg .= " 持续时间 {$duration}.";
-            } else {
-                $this->loginMsg .= ".";
             }
             $this->afterLogin($identity, false, $duration);
         }
