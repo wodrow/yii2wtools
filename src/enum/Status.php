f582d5a075306452ff860ed0389a8d86933fa90c
wodrow-ys-pc <wodrow451611cv@gmail.com>
2019-11-04 16:38:58 +0800

    update

--- src/enum/Status.php
+++ src/enum/Status.php
@@ -12,11 +12,13 @@ namespace wodrow\yii2wtools\enum;
 class Status
 {
     const STATUS_ACTIVE = 10;
+    const STATUS_DEL = -10;
 
     public static function getStatus()
     {
         return [
             self::STATUS_ACTIVE => "正常",
+            self::STATUS_DEL => "删除",
         ];
     }
 }
