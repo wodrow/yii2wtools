c0792449b70e362d52ba92c2b0bb9029972e74bd
wodrow-teclast360 <wodrow451611cv@gmail.com>
2021-04-30 10:12:04 +0800

    add 19 begin for mobile validate

--- src/validators/Mobile.php
+++ src/validators/Mobile.php
@@ -10,7 +10,7 @@ class Mobile extends Validator
     {
         $mobile = $model->$attribute;
         if(strlen($mobile) == "11"){
-            $r = "/^1[345678]{1}\d{9}$/";
+            $r = "/^1[3456789]{1}\d{9}$/";
 //            $r = "/^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/";
             preg_match($r, $mobile, $arr);
             if (!$arr){
